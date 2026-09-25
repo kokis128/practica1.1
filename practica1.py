681bb12005ed77d163b990653f7d4181a0c0a75a
unknown <jorgevalentini76@gmail.com>
2026-09-25 11:02:50 -0300

    agregue el ejercici1

--- practica1.py
+++ practica1.py
@@ -1,3 +1,16 @@
+#ejercicio 1 imprimir el nombre y edad y materia favorita de un alumno
+
+nombre = input("Ingrese su nombre: ")
+materia = input("Ingrese su materia favorita: ")
+edad = int(input("Ingrese su edad: "))
+
+print("Nombre:", nombre)
+print("Materia favorita:", materia)
+print("Edad:", edad)
+
+
+
+
 alumnos = [
 {
 "nombre": "Juan",
